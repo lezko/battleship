77433be88194fe971ea15bd3e68f9c8f962e34aa
Timophey <ulezko.2015@mail.ru>
2023-10-13 00:20:20 +0300

    initial

---
+++ frontend/src/index.tsx
@@ -0,0 +1,14 @@
+import {createRoot} from 'react-dom/client';
+import {StrictMode} from 'react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import App from 'App';
+
+const root = createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+serviceWorkerRegistration.unregister();
